97e33569dd92488fef21c39ef818a19667a078f2
yamamoto <d.yamamoto.amakusa.nakamuraya@gmail.com>
2024-07-03 17:50:12 +0900

    shader.add

--- src/app/utils/App.tsx
+++ src/app/utils/App.tsx
@@ -1,24 +1,52 @@
-'use client'
+'use client';
+
 import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useMemo, useRef } from "react";
+import vertexShader from "./glsl/vertex.glsl";
+import fragmentShader from "./glsl/fragment.glsl";
+import { Color } from "three";
 
 const Scene = () =>{
-    const mesh = useRef()
+    const mesh = useRef();
+
+    const uniforms = useMemo(
+        () => ({
+            u_time:{
+                value:0.0,
+            },
+            u_colorA:{value:new Color("#FFE486")},
+            u_colorB:{value:new Color("#FG65988")},
+        }),
+        []
+    );
+
+    useFrame((state) =>{
+        const {clock} = state;
+        mesh.current.material.uniforms.u_time.value = clock.getElapsedTime();
+    })
 
     return(
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
+        <mesh ref={mesh}
+        position={[0.0,0.0,1.0]}
+        rotation={[Math.PI / 2, 0, 0]}
+        scale={1.0}>
+            <planeGeometry args={[1.0, 1.0, 16.0, 16.0]} />
+            <shaderMaterial
+            fragmentShader = {fragmentShader}
+            vertexShader = {vertexShader}
+            uniforms={uniforms}
+            wireframe
+            />
         </mesh>
     )
 }
 
 export default function App(){
     return(
-        <Canvas camera={{position:[1,1,1]}}>
+        <Canvas camera={{position:[0,0,1]}}>
             <Scene />
-            <OrbitControls />
+            <OrbitControls/>
         </Canvas>
     )
 }
---
+++ src/app/utils/glsl/fragment.glsl
@@ -0,0 +1,8 @@
+uniform vec3 u_colorA;
+uniform vec3 u_colorB;
+varying float vZ;
+
+void main() {
+  vec3 color = mix(u_colorA, u_colorB, vZ * 12.0 + 0.5); 
+  gl_FragColor = vec4(color, 1.0);
+}
